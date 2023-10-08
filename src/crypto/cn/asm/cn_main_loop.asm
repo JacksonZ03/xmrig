0840362b3efa64945b40bd8e7f2c1c5087ac770e
Jackson Zheng <60581068+JacksonZ03@users.noreply.github.com>
2023-10-08 23:40:14 +0100

    Update cn_main_loop.asm

--- src/crypto/cn/asm/cn_main_loop.asm
+++ src/crypto/cn/asm/cn_main_loop.asm
@@ -1,27 +1,13 @@
 _TEXT_CNV2_MAINLOOP SEGMENT PAGE READ EXECUTE
-PUBLIC cnv1_single_mainloop_asm
-PUBLIC cnv1_double_mainloop_asm
 PUBLIC cnv1_quad_mainloop_asm
+PUBLIC cnv1_double_mainloop_asm
+PUBLIC cnv1_single_mainloop_asm
 PUBLIC cnv2_mainloop_ivybridge_asm
-PUBLIC cnv2_mainloop_ryzen_asm
-PUBLIC cnv2_mainloop_bulldozer_asm
 PUBLIC cnv2_double_mainloop_sandybridge_asm
+PUBLIC cnv2_mainloop_bulldozer_asm
+PUBLIC cnv2_mainloop_ryzen_asm
 PUBLIC cnv2_rwz_mainloop_asm
 PUBLIC cnv2_rwz_double_mainloop_asm
-
-ALIGN(64)
-cnv1_single_mainloop_asm PROC
-	INCLUDE cn1/cnv1_single_main_loop.inc
-	ret 0
-	mov eax, 3735929054
-cnv1_single_mainloop_asm ENDP
-
-ALIGN(64)
-cnv1_double_mainloop_asm PROC
-	INCLUDE cn1/cnv1_double_main_loop.inc
-	ret 0
-	mov eax, 3735929054
-cnv1_double_mainloop_asm ENDP
 
 ALIGN(64)
 cnv1_quad_mainloop_asm PROC
@@ -31,6 +17,20 @@ cnv1_quad_mainloop_asm PROC
 cnv1_quad_mainloop_asm ENDP
 
 ALIGN(64)
+cnv1_double_mainloop_asm PROC
+	INCLUDE cn1/cnv1_double_main_loop.inc
+	ret 0
+	mov eax, 3735929054
+cnv1_double_mainloop_asm ENDP
+
+ALIGN(64)
+cnv1_single_mainloop_asm PROC
+	INCLUDE cn1/cnv1_single_main_loop.inc
+	ret 0
+	mov eax, 3735929054
+cnv1_single_mainloop_asm ENDP
+
+ALIGN(64)
 cnv2_mainloop_ivybridge_asm PROC
 	INCLUDE cn2/cnv2_main_loop_ivybridge.inc
 	ret 0
@@ -38,11 +38,11 @@ cnv2_mainloop_ivybridge_asm PROC
 cnv2_mainloop_ivybridge_asm ENDP
 
 ALIGN(64)
-cnv2_mainloop_ryzen_asm PROC
-	INCLUDE cn2/cnv2_main_loop_ryzen.inc
+cnv2_double_mainloop_sandybridge_asm PROC
+	INCLUDE cn2/cnv2_double_main_loop_sandybridge.inc
 	ret 0
 	mov eax, 3735929054
-cnv2_mainloop_ryzen_asm ENDP
+cnv2_double_mainloop_sandybridge_asm ENDP
 
 ALIGN(64)
 cnv2_mainloop_bulldozer_asm PROC
@@ -52,11 +52,18 @@ cnv2_mainloop_bulldozer_asm PROC
 cnv2_mainloop_bulldozer_asm ENDP
 
 ALIGN(64)
-cnv2_double_mainloop_sandybridge_asm PROC
-	INCLUDE cn2/cnv2_double_main_loop_sandybridge.inc
+cnv2_mainloop_ryzen_asm PROC
+	INCLUDE cn2/cnv2_main_loop_ryzen.inc
 	ret 0
 	mov eax, 3735929054
-cnv2_double_mainloop_sandybridge_asm ENDP
+cnv2_mainloop_ryzen_asm ENDP
+
+ALIGN(64)
+cnv2_upx_double_mainloop_zen3_asm PROC
+	INCLUDE cn2/cnv2_upx_double_mainloop_zen3.inc
+	ret 0
+	mov eax, 3735929054
+cnv2_upx_double_mainloop_zen3_asm ENDP
 
 ALIGN(64)
 cnv2_rwz_mainloop_asm PROC
@@ -71,13 +78,6 @@ cnv2_rwz_double_mainloop_asm PROC
 	ret 0
 	mov eax, 3735929054
 cnv2_rwz_double_mainloop_asm ENDP
-
-ALIGN(64)
-cnv2_upx_double_mainloop_zen3_asm PROC
-	INCLUDE cn2/cnv2_upx_double_mainloop_zen3.inc
-	ret 0
-	mov eax, 3735929054
-cnv2_upx_double_mainloop_zen3_asm ENDP
 
 _TEXT_CNV2_MAINLOOP ENDS
 END
